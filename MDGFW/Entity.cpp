7bcd7b6c0de346dbbc75333da84aa38988c5ed99
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-03-09 23:56:46 +0100

    Add Entity

---
+++ MDGFW/Entity.cpp
@@ -0,0 +1,9 @@
+#include <MDGFW\Entity.h>
+
+Entity::Entity()
+{
+}
+
+Entity::~Entity()
+{
+}
---
+++ MDGFW/Entity.h
@@ -0,0 +1,18 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include <MDGFW\sprite.h>
+
+class Entity
+{
+public:
+	virtual ~Entity();
+
+private:
+
+protected:
+	Entity();
+
+};
+
+#endif // !ENTITY_H

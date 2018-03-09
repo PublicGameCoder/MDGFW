13b48596de3f793d3dc22fe002dadccab2ce558d
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-03-09 23:59:13 +0100

    Modify main

--- Workspace/main.cpp
+++ Workspace/main.cpp
@@ -1,47 +1,27 @@
-// Include GLEW
-#include <GL\glew.h>
-
-// Include GLFW
-#include <glfw3.h>
-
-#include <MDGFW/renderer.h>
-#include <MDGFW/camera.h>
-#include <MDGFW/sprite.h>
+#include <MDGFW/Engine.h>
 
 int main( void )
 {
-	Renderer renderer(1280, 720);
+	Engine engine;
+
+	World* world1 = new World();
 
 	Sprite* pencils = new Sprite("assets/pencils.tga");
 	Sprite* kingkong = new Sprite("assets/kingkong.tga");
 	Sprite* rgba = new Sprite("assets/rgba.tga");
 
-	float rot_z = 0.0f;
+	world1->addSprite( pencils );
+	world1->addSprite( kingkong );
+	world1->addSprite( rgba );
 
 	do {
-		// Clear the screen
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Compute the ViewMatrix from keyboard and mouse input (see: camera.h/cpp)
-		computeMatricesFromInputs(renderer.window());
-
-		//glm::vec3 cursor = getCursor(); // from Camera
-		//printf("(%f,%f)\n",cursor.x, cursor.y);
-
-		// Render all Sprites (Sprite*, xpos, ypos, xscale, yscale, rotation)
-		renderer.renderSprite(pencils, 400, 300, 1.0f, 1.0f, 0.0f);
-		renderer.renderSprite(kingkong, 900, 400, 1.0f, 1.0f, 0.0f);
-		renderer.renderSprite(rgba, renderer.width()/2, renderer.height()/2, 3.0f, 3.0f, rot_z);
-		rot_z += 0.03f;
-
-		// Swap buffers
-		glfwSwapBuffers(renderer.window());
-		glfwPollEvents();
+		engine.run(world1);
 
 	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(renderer.window(), GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(renderer.window()) == 0 );
-
+	while( engine.isRunning() || !glfwWindowShouldClose(engine.getWindow()) == 0 );
+	
+	delete world1;
 	delete pencils;
 	delete kingkong;
 	delete rgba;

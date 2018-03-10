c03696eacd71426b374107ccd67e9fc01345e4ba
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-03-10 16:07:33 +0100

    Implement Entity class

--- MDGFW/Entity.cpp
+++ MDGFW/Entity.cpp
@@ -1,9 +1,42 @@
 #include <MDGFW\Entity.h>
 
-Entity::Entity()
-{
+Entity::Entity() {
+	_sprite = nullptr;
 }
 
-Entity::~Entity()
-{
+Entity::~Entity() {
+	for each (Entity* child in _childs)
+	{
+		if ( child != nullptr ) {
+			delete child;
+		}
+	}
+	flushSprite();
+}
+
+void Entity::update() {
+
+}
+
+void Entity::addSprite(std::string filePath) {
+	flushSprite();
+	_sprite = new Sprite( filePath );
+}
+
+void Entity::addChild( Entity* entity ) {
+	_childs.push_back( entity );
+}
+
+Sprite* Entity::getSprite() {
+	return this->_sprite;
+}
+
+std::vector<Entity*> Entity::getChilds() {
+	return _childs;
+};
+
+void Entity::flushSprite() {
+	if ( _sprite != nullptr ) {
+		delete _sprite;
+	}
 }
--- MDGFW/Entity.h
+++ MDGFW/Entity.h
@@ -1,18 +1,31 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include <vector>
+#include <MDGFW\VectorX.h>
 #include <MDGFW\sprite.h>
+#include <MDGFW\SHTime.h>
 
 class Entity
 {
+
 public:
-	virtual ~Entity();
-
-private:
-
-protected:
 	Entity();
+	virtual ~Entity();
+	virtual void update();
+	void addSprite(std::string filePath);
+	void addChild( Entity* entity );
+	void flushSprite();
+	Sprite* getSprite();
+	Vector3 position;
+	Vector3 rotation;
+	Vector3 scale;
+	std::vector<Entity*> getChilds();
+private:
+	Sprite* _sprite;
 
+	std::vector<Entity*> _childs;
+protected:
 };
 
 #endif // !ENTITY_H

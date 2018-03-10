5ffa287a49f864965877906e2163a494b23491e8
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-03-10 17:56:57 +0100

    Implement a list with lines and Add entities location in worldspace and Implement tracking parent

--- MDGFW/Entity.cpp
+++ MDGFW/Entity.cpp
@@ -1,7 +1,10 @@
 #include <MDGFW\Entity.h>
 
 Entity::Entity() {
+	position = Vector3();
+	_worldPosition = position;
 	_sprite = nullptr;
+	_parent = nullptr;
 }
 
 Entity::~Entity() {
@@ -11,11 +14,24 @@ Entity::~Entity() {
 			delete child;
 		}
 	}
+
+	for each (Line* line in _lines) {
+		if ( line != nullptr ) {
+			delete line;
+		}
+	}
+
 	flushSprite();
 }
 
 void Entity::update() {
+	calcWorldPosition();
+}
 
+void Entity::calcWorldPosition() {
+	_worldPosition = position;
+	if ( _parent == nullptr )return;
+	_worldPosition += _parent->_worldPosition;
 }
 
 void Entity::addSprite(std::string filePath) {
@@ -25,6 +41,12 @@ void Entity::addSprite(std::string filePath) {
 
 void Entity::addChild( Entity* entity ) {
 	_childs.push_back( entity );
+	entity->_parent = this;
+	entity->_worldPosition += _worldPosition;
+}
+
+void Entity::addLine( Line* line ) {
+	_lines.push_back( line );
 }
 
 Sprite* Entity::getSprite() {
@@ -33,7 +55,15 @@ Sprite* Entity::getSprite() {
 
 std::vector<Entity*> Entity::getChilds() {
 	return _childs;
-};
+}
+
+std::vector<Line*> Entity::getLines() {
+	return _lines;
+}
+
+Vector3 Entity::getWorldPosition() {
+	return _worldPosition;
+}
 
 void Entity::flushSprite() {
 	if ( _sprite != nullptr ) {
--- MDGFW/Entity.h
+++ MDGFW/Entity.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <MDGFW\VectorX.h>
+#include <MDGFW\Line.h>
 #include <MDGFW\sprite.h>
 #include <MDGFW\SHTime.h>
 
@@ -15,16 +16,24 @@ public:
 	virtual void update();
 	void addSprite(std::string filePath);
 	void addChild( Entity* entity );
+	void addLine( Line* line );
 	void flushSprite();
 	Sprite* getSprite();
 	Vector3 position;
 	Vector3 rotation;
 	Vector3 scale;
 	std::vector<Entity*> getChilds();
+	std::vector<Line*> getLines();
+	Vector3 getWorldPosition();
 private:
+	Entity* _parent;
+	Vector3 _worldPosition;
 	Sprite* _sprite;
 
 	std::vector<Entity*> _childs;
+	std::vector<Line*> _lines;
+
+	void calcWorldPosition();
 protected:
 };
 

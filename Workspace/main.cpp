3e5da84f0f76ca81ff9c2a3afad0785d578068a3
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-03-20 09:59:05 +0100

    Modified main.cpp

--- Workspace/main.cpp
+++ Workspace/main.cpp
@@ -1,4 +1,5 @@
 #include <Workspace\RGBAObj.h>
+#include <Workspace\StickFigure.h>
 #include <MDGFW/Engine.h>
 
 int main( void )
@@ -7,16 +8,14 @@ int main( void )
 
 	World* world1 = new World();
 
-	Line* line = new Line();
-
 	Entity* pencils = new Entity();
-	pencils->addSprite( "assets/pencils.tga" );
+	pencils->addSprite( "assets/awesomeface.png" );
 	pencils->position = Vector2( 400, 300 );
 	pencils->scale = Vector2( 1, 1, 1 );
 	pencils->rotation = Vector3();
 
 	Entity* kingkong = new Entity();
-	kingkong->addSprite( "assets/kingkong.tga" );
+	kingkong->addSprite( "assets/wall.jpg" );
 	kingkong->position = Vector2( 900, 400 );
 	kingkong->scale = Vector2( 1, 1 );
 	kingkong->rotation = Vector3();
@@ -27,17 +26,19 @@ int main( void )
 	rgba->scale = Vector2( 1, 1 );
 	rgba->rotation = Vector3();
 
-	Entity* bezierCurve = new Entity();
-	bezierCurve->position = Vector2( MINSWIDTH / 2, (MINSHEIGHT / 2) );
-	pencils->addLine( line );
+	StickFigure* stickFigure = new StickFigure();
+	stickFigure->position = Vector2( 400, 300 );
+	stickFigure->scale = Vector2( 1, 1, 1 );
+	stickFigure->rotation = Vector3();
 
 	world1->addChild( pencils );
 	world1->addChild( kingkong );
 	world1->addChild( rgba );
-	world1->addChild( bezierCurve );
+	world1->addChild( stickFigure );
 
 	do {
 		engine.run(world1);
+
 	} // Check if the ESC key was pressed or the window was closed
 	while( engine.isRunning() || !glfwWindowShouldClose(engine.getWindow()) == 0 );
 	
